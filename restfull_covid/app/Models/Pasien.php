caa10205e2c5905c5b0a05b5eb147b65511d666e
vy09 <luvymuhammad09@gmail.com>
2022-11-19 18:39:25 +0700

    membuat model dan migration

---
+++ restfull_covid/app/Models/Pasien.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pasien extends Model
+{
+    use HasFactory;
+    protected $fillable=['name','phone','address','status','in_date_at','out_date_at'];
+}
