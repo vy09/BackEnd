6e8b0f6fb06cfed51e28ee93277992460a4ccd49
Luvy Muhammad <luvymuhammad09@gmail.com>
2022-12-05 20:52:38 +0700

    Update Praktikum-4

--- Praktikum-4/Task/app.js
+++ Praktikum-4/Task/app.js
@@ -6,27 +6,27 @@
  * @hint - Gunakan Destructing Object
  */
 
- const {
-    index,
-    store,
-    update,
-    destroy,
-  } = require("./controllers/Fruitcontroller");
-  
-  /**
-   * NOTES:
-   * - Fungsi main tidak perlu diubah
-   * - Jalankan program: nodejs app.js
-   */
-  const main = () => {
-    console.log("Method index - Menampilkan Buah");
-    index();
-    console.log("\nMethod store - Menambahkan buah Banana");
-    store("");
-    console.log("\nMethod update - Update data 0 menjadi coconut");
-    update(0, "coconut");
-    console.log("\nMethod destroy - Menghapus data 0");
-    destroy(0);
-  };
-  
-  main();
+const {
+  index,
+  store,
+  update,
+  destroy,
+} = require("./Controllers/FruitController");
+
+/**
+ * NOTES:
+ * - Fungsi main tidak perlu diubah
+ * - Jalankan program: nodejs app.js
+ */
+const main = () => {
+  console.log("Method index - Menampilkan Buah");
+  index();
+  console.log("\nMethod store - Menambahkan Buah Banana");
+  store("Banana");
+  console.log("\nMethod update - Update data 0 menjadi Coconut");
+  update(0, "Coconut");
+  console.log("\nMethod destroy - Menghapus data 0");
+  destroy(0);
+};
+
+main();
--- Praktikum-4/Task/data/Fruits.js
+++ Praktikum-4/Task/data/Fruits.js
@@ -3,7 +3,14 @@
  * - Buat array yang berisi data buah.
  * - Refactor variable ke ES6 Variable.
  */
- const fruits = ["Apple", "Watermelon", "Blueberry", "Strawberry", "Coconut", "Banana",];
+const fruits = [
+  "Apple",
+  "Watermelon",
+  "Blueberry",
+  "Strawberry",
+  "Coconut",
+  "Banana",
+];
 
- // TODO 2: export variable fruits
- module.exports = Fruits;
+// TODO 2: export variable fruits
+module.exports = fruits;
